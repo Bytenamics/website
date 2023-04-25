023106979ec71a81197340b7693bf44ec76c6b1b
Anish-Shobith <anish.shobith19@gmail.com>
2023-04-25 13:11:09 +0530

    add: typings for lottie and add svelte
    Signed-off-by: Anish-Shobith <anish.shobith19@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,44 +6,41 @@ import preact from '@astrojs/preact';
 import image from '@astrojs/image';
 import compress from 'astro-compress';
 import { SITE } from './src/ts/config';
+import svelte from "@astrojs/svelte";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// https://astro.build/config
 export default defineConfig({
   site: SITE.origin,
   base: SITE.basePathname,
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
   output: 'static',
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    compress({
-      css: true,
-      html: {
-        removeAttributeQuotes: false,
-      },
-      img: true,
-      js: true,
-      svg: true,
-    }),
-    preact({
-      compact: true,
-    }),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), compress({
+    css: true,
+    html: {
+      removeAttributeQuotes: false
+    },
+    img: true,
+    js: true,
+    svg: true
+  }), preact({
+    compact: true
+  }), svelte()],
   vite: {
     ssr: {
-      external: ['svgo'],
+      external: ['svgo']
     },
     assetsInclude: '**/*.lottie',
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, './src'),
-      },
-    },
-  },
+        '~': path.resolve(__dirname, './src')
+      }
+    }
+  }
 });
